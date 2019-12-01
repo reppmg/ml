76c96f65f175c4e1c5a2c7e3431b3e2dc948487d
maksimrepp <reppmg@gmail.com>
2019-12-01 03:01:05 +0300

    input

--- hw.py
+++ hw.py
@@ -1,1 +1,8 @@
-print("hello world!")
+import numpy as np
+
+n, m, k = list(map(int, str(input()).split(" ")))
+classes = list(map(int, str(input()).split(" ")))
+arange = np.arange(n)
+data = np.array([np.array(classes), arange], np.int32)
+
+
