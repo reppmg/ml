33801d7c3418d131c5490d888deb831aee0efb1f
maksimrepp <reppmg@gmail.com>
2019-12-01 04:51:53 +0300

    passed

--- hw.py
+++ hw.py
@@ -1,8 +1,16 @@
-import numpy as np
-
 n, m, k = list(map(int, str(input()).split(" ")))
 classes = list(map(int, str(input()).split(" ")))
-arange = np.arange(n)
-data = np.array([np.array(classes), arange], np.int32)
-
-
+new_ar = list(zip(classes, [i for i in range(len(classes))]))
+new_ar.sort()
+answer = []
+for i in range(0, k):
+    answer.append([])
+i = 0
+for (cls, index) in new_ar:
+    answer[i % k].append(index)
+    i += 1
+for line in answer:
+    print(len(line), end=" ")
+    for index in line:
+        print(index + 1, end=" ")
+    print()
