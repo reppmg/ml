b77772da6c414662f5eb571635ec48a3e2f4728d
maksimrepp <reppmg@gmail.com>
2019-12-14 01:09:01 +0300

    2: done

--- ml2.py
+++ ml2.py
@@ -20,6 +20,24 @@ def f_score(recall, precision):
     return 2 * recall * precision / (recall + precision)
 
 
+def recall(tp, fn):
+    if tp == 0:
+        return 0
+    return tp/(tp+fn)
+
+
+def precision(tp, fp):
+    if tp == 0:
+        return 0
+    return tp/(tp+fp)
+
+
+def divide_or_zero(numerator, denominator):
+    if numerator == 0:
+        return 0
+    return numerator / denominator
+
+
 k = int(str(input()))
 classes = []
 for i in range(k):
@@ -29,12 +47,12 @@ C = list(map(lambda x: sum(x), classes))
 fps = list(map(lambda x: fp(classes, x), range(k)))
 tps = list(map(lambda x: tp(classes, x), range(k)))
 fns = list(map(lambda x: fn(classes, x), range(k)))
-recalls = list(map(lambda x: tps[x] / (tps[x] + fns[x]), range(k)))
-precisions = list(map(lambda x: tps[x] / (tps[x] + fps[x]), range(k)))
+recalls = list(map(lambda x: recall(tps[x], fns[x]), range(k)))
+precisions = list(map(lambda x: precision(tps[x], fps[x]), range(k)))
 f_scores = list(map(lambda x: f_score(recalls[x], precisions[x]), range(k)))
-f_micro = sum(map(lambda x: f_scores[x] * C[x], range(k))) / All
-p_w = sum(map(lambda x: tps[x] * C[x] / (tps[x] + fps[x]), range(k))) / All
-r_w = sum(tps) / All
+f_micro = divide_or_zero(sum(map(lambda x: f_scores[x] * C[x], range(k))), All)
+p_w = divide_or_zero(sum(map(lambda x: C[x] * precision(tps[x], fps[x]), range(k))), All)
+r_w = divide_or_zero(sum(tps), All)
 f_macro = f_score(r_w, p_w)
 print(f_macro)
 print(f_micro)
