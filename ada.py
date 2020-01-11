a2975f5651cdf74fcc72811aaa12336354821cde
maksimrepp <reppmg@gmail.com>
2020-01-11 21:04:27 +0300

    plane sequence

--- ada.py
+++ ada.py
@@ -45,8 +45,7 @@ class_names = "AB"
 xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                      np.arange(y_min, y_max, plot_step))
 
-fig = plt.figure(1)
-
+plt.figure(1)
 for i in range(1, ESTIMATORS):
     clf = AdaBoostClassifier(n_estimators=i)
     clf.fit(x, y)
@@ -54,14 +53,14 @@ for i in range(1, ESTIMATORS):
     z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
     z = z.reshape(xx.shape)
 
+    plt.clf()
     cs = plt.contourf(xx, yy, z, cmap=plt.cm.Paired)
     plt.axis("tight")
-
     # for class_value, class_name, class_color in zip(range(2), class_names, plot_colors):
     #     idx = np.where(y == class_value)
     #     plt.scatter(x[idx, 0], x[idx, 1],w
     #                 c=class_color, cmap=plt.cm.Paired,
     #                 s=20, edgecolor='k',
     #                 label="Class %s" % class_name)
-    plt.show()
-    time.sleep(0.2)
+
+    plt.pause(0.3)
